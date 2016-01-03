8ac6ab1222b4d3bd626dd37175172f301df5ff5a
sirex <sirexas@gmail.com>
2016-01-03 13:28:49 +0200

    Places autocomplete widget styling

--- savanoriai/core/autocomplete_light_registry.py
+++ savanoriai/core/autocomplete_light_registry.py
@@ -1,3 +1,5 @@
+from django.utils.translation import ugettext_lazy as _
+
 import autocomplete_light.shortcuts as al
 
 from savanoriai.core.models import Place
@@ -6,13 +8,12 @@ from savanoriai.core.models import Place
 class CityAutocomplete(al.AutocompleteModelBase):
     model = Place
     search_fields = ['^wikipedia_title']
+    limit_choices = 8
+    split_words = False
     order_by = '-population'
     attrs = {
         'data-autocomplete-minimum-characters': 1,
-    }
-    widget_attrs = {
-        'data-widget-maximum-values': 16,
-        'class': 'modern-style',
+        'placeholder': _('Įveskite kelias vietovės raides'),
     }
 
 al.register(CityAutocomplete)
